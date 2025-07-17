503c55f3feff2aed51dd437ba8bec6c35b16a506
Howler100 <jaxoncall@gmail.com>
2025-07-17 09:58:24 -0600

    Create missionimpossible-movies-config.js

---
+++ configs/configs/missionimpossible-movies-config.js
@@ -0,0 +1,19 @@
+// configs/missionimpossible-movies-config.js
+window.MovieRankerConfig = {
+  theme: {
+    // put your MI background image here:
+    background: 'assets/backgrounds/mission-impossible-background.jpg',
+    // import a matching font in your HTML head (see step 3) and reference it here:
+    fontFamily: "'Russo One', sans-serif"
+  },
+  movies: [
+    { title: 'Mission Impossible (1996)',                           poster: 'assets/posters/missionimpossible/movies/Mission Impossible (1996).jpg' },
+    { title: 'Mission Impossible II (2000)',                        poster: 'assets/posters/missionimpossible/movies/Mission Impossible II (2000).jpg' },
+    { title: 'Mission Impossible III (2006)',                       poster: 'assets/posters/missionimpossible/movies/Mission Impossible III (2006).jpg' },
+    { title: 'Mission Impossible - Ghost Protocol (2011)',          poster: 'assets/posters/missionimpossible/movies/Mission Impossible - Ghost Protocol (2011).jpg' },
+    { title: 'Mission Impossible - Rogue Nation (2015)',            poster: 'assets/posters/missionimpossible/movies/Mission Impossible - Rogue Nation (2015).jpg' },
+    { title: 'Mission Impossible - Fallout (2018)',                 poster: 'assets/posters/missionimpossible/movies/Mission Impossible - Fallout (2018).jpg' },
+    { title: 'Mission Impossible - Dead Reckoning Part One (2023)',  poster: 'assets/posters/missionimpossible/movies/Mission Impossible - Dead Reckoning Part One (2023).jpg' },
+    { title: 'Mission Impossible - The Final Reckoning (2025)',     poster: 'assets/posters/missionimpossible/movies/Mission Impossible - The Final Reckoning (2025).jpg' }
+  ]
+};
