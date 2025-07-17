040c82904a2d806528af1e3f43291722228f523b
Howler100 <jaxoncall@gmail.com>
2025-07-17 14:15:00 -0600

    Create harry potter-movies-config.js

---
+++ configs/harry potter-movies-config.js
@@ -0,0 +1,33 @@
+// configs/harrypotter-movies-config.js
+window.MovieRankerConfig = {
+  theme: {
+    background: 'assets/backgrounds/harry-potter-background.jpg',
+    fontFamily: "'Cinzel', serif"
+  },
+  movies: [
+    { title: "Harry Potter and the Philosopher's Stone (2001)",
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Philosophers Stone (2001).png' },
+    { title: 'Harry Potter and the Chamber of Secrets (2002)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Chamber of Secrets (2002).png' },
+    { title: 'Harry Potter and the Prisoner of Azkaban (2004)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Prisoner of Azkaban (2004).png' },
+    { title: 'Harry Potter and the Goblet of Fire (2005)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Goblet of Fire (2005).png' },
+    { title: 'Harry Potter and the Order of the Phoenix (2007)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Order of the Phoenix (2007).png' },
+    { title: 'Harry Potter and the Half-Blood Prince (2009)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Half-Blood Prince (2009).png' },
+    { title: 'Harry Potter and the Deathly Hallows Part 1 (2010)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Deathly Hallows Part 1 (2010).png' },
+    { title: 'Harry Potter and the Deathly Hallows Part 2 (2011)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter and the Deathly Hallows Part 2 (2011).png' },
+    { title: 'Fantastic Beasts and Where to Find Them (2016)',
+      poster: 'assets/posters/harrypotter/movies/Fantastic Beasts and Where to Find Them (2016).png' },
+    { title: 'Fantastic Beasts The Crimes of Grindelwald (2018)',
+      poster: 'assets/posters/harrypotter/movies/Fantastic Beasts The Crimes of Grindelwald (2018).png' },
+    { title: 'Fantastic Beasts The Secrets of Dumbledore (2022)',
+      poster: 'assets/posters/harrypotter/movies/Fantastic Beasts The Secrets of Dumbledore (2022).png' },
+    { title: 'Harry Potter 20th Anniversary Return to Hogwarts (2022)',
+      poster: 'assets/posters/harrypotter/movies/Harry Potter 20th Anniversary Return to Hogwarts (2022).png' }
+  ]
+};
