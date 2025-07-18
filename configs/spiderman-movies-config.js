7995ea598b46fe3f229de244050ae93386117c7c
Howler100 <jaxoncall@gmail.com>
2025-07-18 12:03:41 -0600

    Create spiderman-movies-config.js

---
+++ configs/spiderman-movies-config.js
@@ -0,0 +1,31 @@
+// configs/spiderman-movies-config.js
+window.MovieRankerConfig = {
+  theme: {
+    background: 'assets/backgrounds/spider-man-background.jpg',
+    fontFamily: "'Bangers', cursive"
+  },
+  movies: [
+    { title: 'Spider-Man (2002)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man (2002).jpg' },
+    { title: 'Spider-Man 2 (2004)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man 2 (2004).jpg' },
+    { title: 'Spider-Man 3 (2007)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man 3 (2007).jpg' },
+    { title: 'The Amazing Spider-Man (2012)',
+      poster: 'assets/posters/spiderman/movies/The Amazing Spider-Man (2012).jpg' },
+    { title: 'The Amazing Spider-Man 2 (2014)',
+      poster: 'assets/posters/spiderman/movies/The Amazing Spider-Man 2 (2014).jpg' },
+    { title: 'Spider-Man Homecoming (2017)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man Homecoming (2017).jpg' },
+    { title: 'Spider-Man Into the Spider-Verse (2018)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man Into the Spider-Verse (2018).jpg' },
+    { title: 'Spider-Man Far From Home (2019)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man Far From Home (2019).jpg' },
+    { title: 'Spider-Man No Way Home (2021)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man No Way Home (2021).jpg' },
+    { title: 'Spider-Man Across the Spider-Verse (2023)',
+      poster: 'assets/posters/spiderman/movies/Spider-Man Across the Spider-Verse (2023).jpg' },
+    { title: 'Your Friendly Neighborhood Spider-Man (2025) - Season 1',
+      poster: 'assets/posters/spiderman/movies/Your Friendly Neighborhood Spider-Man (2025) - Season 1.jpg' }
+  ]
+};
