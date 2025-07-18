a75b1d3d054d00f13931843beaaca2dd721b5bc9
Howler100 <jaxoncall@gmail.com>
2025-07-18 16:00:49 -0600

    Update mcutv-tv-config.js

--- configs/mcutv-tv-config.js
+++ configs/mcutv-tv-config.js
@@ -7,59 +7,59 @@ window.MovieRankerConfig = {
   movies: [
     {
       title: 'WandaVision (2021)',
-      poster: 'assets/posters/marveltv/movies/WandaVision (2021) - Season 1.jpg'
+      poster: 'assets/posters/mcutv/tv/WandaVision (2021) - Season 1.jpg'
     },
     {
       title: 'The Falcon and the Winter Soldier (2021)',
-      poster: 'assets/posters/marveltv/movies/The Falcon and the Winter Soldier (2021).jpg'
+      poster: 'assets/posters/mcutv/tv/The Falcon and the Winter Soldier (2021).jpg'
     },
     {
       title: 'Loki (2021)',
-      poster: 'assets/posters/marveltv/movies/Loki (2021).jpg'
+      poster: 'assets/posters/mcutv/tv/Loki (2021).jpg'
     },
     {
       title: 'What If…? (2021)',
-      poster: 'assets/posters/marveltv/movies/What If…? (2021).png'
+      poster: 'assets/posters/mcutv/tv/What If…? (2021).png'
     },
     {
       title: 'Hawkeye (2021)',
-      poster: 'assets/posters/marveltv/movies/Hawkeye (2021).jpg'
+      poster: 'assets/posters/mcutv/tv/Hawkeye (2021).jpg'
     },
     {
       title: 'Moon Knight (2022)',
-      poster: 'assets/posters/marveltv/movies/Moon Knight (2022).jpg'
+      poster: 'assets/posters/mcutv/tv/Moon Knight (2022).jpg'
     },
     {
       title: 'Ms. Marvel (2022)',
-      poster: 'assets/posters/marveltv/movies/Ms. Marvel (2022).png'
+      poster: 'assets/posters/mcutv/tv/Ms. Marvel (2022).png'
     },
     {
       title: 'I Am Groot (2022)',
-      poster: 'assets/posters/marveltv/movies/I Am Groot (2022).jpg'
+      poster: 'assets/posters/mcutv/tv/I Am Groot (2022).jpg'
     },
     {
       title: 'She-Hulk: Attorney at Law (2022)',
-      poster: 'assets/posters/marveltv/movies/She-Hulk Attorney at Law (2022).jpg'
+      poster: 'assets/posters/mcutv/tv/She-Hulk Attorney at Law (2022).jpg'
     },
     {
       title: 'Secret Invasion (2023)',
-      poster: 'assets/posters/marveltv/movies/Secret Invasion (2023) - Season 1.jpg'
+      poster: 'assets/posters/mcutv/tv/Secret Invasion (2023) - Season 1.jpg'
     },
     {
       title: 'Echo (2024)',
-      poster: 'assets/posters/marveltv/movies/Echo (2024).png'
+      poster: 'assets/posters/mcutv/tv/Echo (2024).png'
     },
     {
       title: 'Agatha All Along (2024)',
-      poster: 'assets/posters/marveltv/movies/Agatha All Along (2024).jpg'
+      poster: 'assets/posters/mcutv/tv/Agatha All Along (2024).jpg'
     },
     {
       title: 'Ironheart (2025)',
-      poster: 'assets/posters/marveltv/movies/Ironheart (2025).png'
+      poster: 'assets/posters/mcutv/tv/Ironheart (2025).png'
     },
     {
       title: 'Daredevil: Born Again (2025)',
-      poster: 'assets/posters/marveltv/movies/Daredevil_ Born Again (2025).png'
+      poster: 'assets/posters/mcutv/tv/Daredevil_ Born Again (2025).png'
     }
   ]
 };
