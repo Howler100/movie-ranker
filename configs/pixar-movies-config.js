bb10b9c880762cdc540671a03fd1c3a20dbbb489
Howler100 <jaxoncall@gmail.com>
2025-07-31 11:32:23 -0600

    Create pixar-movies-config.js

---
+++ configs/pixar-movies-config.js
@@ -0,0 +1,35 @@
+// configs/pixar-movies-config.js
+window.MovieRankerConfig = {
+  theme: {
+    background: 'assets/backgrounds/pixar-background.jpg',
+    fontFamily: "'Luckiest Guy', sans-serif"
+  },
+  movies: [
+    { title: 'Toy Story (1995)',                  poster: 'assets/posters/pixar/movies/Toy Story (1995).png' },
+    { title: 'Toy Story 2 (1999)',                poster: 'assets/posters/pixar/movies/Toy Story 2 (1999).png' },
+    { title: 'Monsters, Inc. (2001)',              poster: 'assets/posters/pixar/movies/Monsters, Inc. (2001).png' },
+    { title: 'Finding Nemo (2003)',                poster: 'assets/posters/pixar/movies/Finding Nemo (2003).png' },
+    { title: 'The Incredibles (2004)',             poster: 'assets/posters/pixar/movies/The Incredibles (2004).png' },
+    { title: 'Cars (2006)',                        poster: 'assets/posters/pixar/movies/Cars (2006).png' },
+    { title: 'Ratatouille (2007)',                 poster: 'assets/posters/pixar/movies/Ratatouille (2007).png' },
+    { title: 'WALL-E (2008)',                      poster: 'assets/posters/pixar/movies/WALLE (2008).png' },
+    { title: 'Up (2009)',                          poster: 'assets/posters/pixar/movies/Up (2009).png' },
+    { title: 'Toy Story 3 (2010)',                 poster: 'assets/posters/pixar/movies/Toy Story 3 (2010).png' },
+    { title: 'Cars 2 (2011)',                      poster: 'assets/posters/pixar/movies/Cars 2 (2011).png' },
+    { title: 'Brave (2012)',                       poster: 'assets/posters/pixar/movies/Brave (2012).png' },
+    { title: 'Monsters University (2013)',         poster: 'assets/posters/pixar/movies/Monsters University (2013).png' },
+    { title: 'Inside Out (2015)',                  poster: 'assets/posters/pixar/movies/Inside Out (2015).png' },
+    { title: 'The Good Dinosaur (2015)',           poster: 'assets/posters/pixar/movies/The Good Dinosaur (2015).png' },
+    { title: 'Finding Dory (2016)',                poster: 'assets/posters/pixar/movies/Finding Dory (2016).png' },
+    { title: 'Cars 3 (2017)',                      poster: 'assets/posters/pixar/movies/Cars 3 (2017).png' },
+    { title: 'Coco (2017)',                        poster: 'assets/posters/pixar/movies/Coco (2017).png' },
+    { title: 'Incredibles 2 (2018)',               poster: 'assets/posters/pixar/movies/Incredibles 2 (2018).png' },
+    { title: 'Onward (2020)',                      poster: 'assets/posters/pixar/movies/Onward (2020).jpeg' },
+    { title: 'Soul (2020)',                        poster: 'assets/posters/pixar/movies/Soul (2020).jpeg' },
+    { title: 'Luca (2021)',                        poster: 'assets/posters/pixar/movies/Luca (2021).jpg' },
+    { title: 'Turning Red (2022)',                 poster: 'assets/posters/pixar/movies/Turning Red (2022).png' },
+    { title: 'Lightyear (2022)',                   poster: 'assets/posters/pixar/movies/Lightyear (2022).jpg' },
+    { title: 'Elemental (2023)',                   poster: 'assets/posters/pixar/movies/Elemental (2023).jpg' },
+    { title: 'Inside Out 2 (2024)',                poster: 'assets/posters/pixar/movies/Inside Out 2 (2024).jpg' }
+  ]
+};
